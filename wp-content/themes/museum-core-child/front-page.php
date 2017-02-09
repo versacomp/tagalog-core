a708f38baad90ae21cf1bbb522732ed267d89c22
Mitchell Lipayon <mitch.lipayon@versacomputer.com>
2017-02-08 23:42:06 -0500

    add front-page and goolge oauth plugin

---
+++ wp-content/themes/museum-core-child/front-page.php
@@ -0,0 +1,19 @@
+<?php
+/* Front Page Template */
+get_header();
+?>
+
+<div class="home">
+	<div class="container">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">Panel title</h3>
+			</div>
+			<div class="panel-body">
+				Panel content
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
