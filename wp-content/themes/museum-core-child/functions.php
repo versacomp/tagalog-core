2a2ba532d20b19bc174ee213508a4cfd3851275f
Mitchell Lipayon <mitch.lipayon@versacomputer.com>
2017-02-09 03:26:27 -0500

    remove dashboard widgets

--- wp-content/themes/museum-core-child/functions.php
+++ wp-content/themes/museum-core-child/functions.php
@@ -29,3 +29,23 @@ function annointed_admin_bar_remove() {
 }
 add_action('wp_before_admin_bar_render', 'annointed_admin_bar_remove', 0);
 
+
+
+function remove_dashboard_widgets () {
+
+	remove_meta_box('dashboard_quick_press','dashboard','side'); //Quick Press widget
+	remove_meta_box('dashboard_recent_drafts','dashboard','side'); //Recent Drafts
+	remove_meta_box('dashboard_primary','dashboard','side'); //WordPress.com Blog
+	remove_meta_box('dashboard_secondary','dashboard','side'); //Other WordPress News
+	remove_meta_box('dashboard_incoming_links','dashboard','normal'); //Incoming Links
+	remove_meta_box('dashboard_plugins','dashboard','normal'); //Plugins
+	remove_meta_box('dashboard_right_now','dashboard', 'normal'); //Right Now
+	remove_meta_box('rg_forms_dashboard','dashboard','normal'); //Gravity Forms
+	remove_meta_box('dashboard_recent_comments','dashboard','normal'); //Recent Comments
+	remove_meta_box('icl_dashboard_widget','dashboard','normal'); //Multi Language Plugin
+	remove_meta_box('dashboard_activity','dashboard', 'normal'); //Activity
+	remove_action('welcome_panel','wp_welcome_panel');
+
+}
+
+add_action('wp_dashboard_setup', 'remove_dashboard_widgets');
