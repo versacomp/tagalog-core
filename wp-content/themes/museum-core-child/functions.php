13b9ef75f51639fa47c4318511706631d7ae6cbc
Mitchell Lipayon <mitch.lipayon@versacomputer.com>
2017-02-09 00:50:27 -0500

    force login to view site

---
+++ wp-content/themes/museum-core-child/functions.php
@@ -0,0 +1,21 @@
+<?php
+
+// Force login to view site
+function v_getUrl() {
+	$url  = isset( $_SERVER['HTTPS'] ) && 'on' === $_SERVER['HTTPS'] ? 'https' : 'http';
+	$url .= '://' . $_SERVER['SERVER_NAME'];
+	$url .= in_array( $_SERVER['SERVER_PORT'], array('80', '443') ) ? '' : ':' . $_SERVER['SERVER_PORT'];
+	$url .= $_SERVER['REQUEST_URI'];
+	return $url;
+}
+function v_forcelogin() {
+	if( !is_user_logged_in() ) {
+		$url = v_getUrl();
+		$whitelist = apply_filters('v_forcelogin_whitelist', array());
+		$redirect_url = apply_filters('v_forcelogin_redirect', $url);
+		if( preg_replace('/\?.*/', '', $url) != preg_replace('/\?.*/', '', wp_login_url()) && !in_array($url, $whitelist) ) {
+			wp_safe_redirect( wp_login_url( $redirect_url ), 302 ); exit();
+		}
+	}
+}
+add_action('init', 'v_forcelogin');
