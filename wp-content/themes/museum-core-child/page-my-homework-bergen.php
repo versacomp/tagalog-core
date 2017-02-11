a6ab3757e8f7b839f3cc9fe17d9c0e8c63133cf1
Mitch Lipayon <mitch.lipayon@versacomputer.com>
2017-02-11 18:57:14 -0500

    create custom template for Bergen homework page

---
+++ wp-content/themes/museum-core-child/page-my-homework-bergen.php
@@ -0,0 +1,12 @@
+<?php
+/*
+	Page template for My-Homework-Bergen
+*/
+
+$ap_core_content = ap_core_get_which_content(); ?>
+    <div class="content col-md-9 <?php echo esc_attr( $ap_core_content ) ?>">
+
+        <?php get_template_part('parts/content', 'page'); ?>
+
+    </div>
+<?php get_footer(); ?>
