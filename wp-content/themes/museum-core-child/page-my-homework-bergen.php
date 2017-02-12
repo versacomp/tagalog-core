f7acea3fa105d61a04b983c662164a63039378a5
Mitch Lipayon <mitch.lipayon@versacomputer.com>
2017-02-11 19:00:22 -0500

    create custom template for Bergen homework page

--- wp-content/themes/museum-core-child/page-my-homework-bergen.php
+++ wp-content/themes/museum-core-child/page-my-homework-bergen.php
@@ -3,7 +3,6 @@
 	Page template for My-Homework-Bergen
 */
 get_header();
-tha_content_before();
 $ap_core_content = ap_core_get_which_content(); ?>
     <div class="content col-md-9 <?php echo esc_attr( $ap_core_content ) ?>">
 
