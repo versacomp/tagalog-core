82a271760b58094e6db61cada92f5824334b414f
Mitch Lipayon <mitch.lipayon@versacomputer.com>
2017-02-11 19:23:07 -0500

    create custom template for Bergen homework page

--- wp-content/themes/museum-core-child/page-my-homework-bergen.php
+++ wp-content/themes/museum-core-child/page-my-homework-bergen.php
@@ -8,8 +8,6 @@ global $post;
 
 $page_slug = get_post_meta($post->ID, 'homework_slug', true);
 
-echo "page_slug: " . $page_slug;
-
 $args = array(
     'name'        => $page_slug,
     'post_type'   => 'post',
@@ -22,14 +20,12 @@ $my_posts = get_posts($args);
 
 <?php if( $my_posts ) :?>
 
-    $ap_core_content = ap_core_get_which_content(); ?>
+    <?php $ap_core_content = ap_core_get_which_content(); ?>
     <div class="content col-md-9 <?php echo esc_attr( $ap_core_content ) ?>">
 
         <?php echo $my_posts[0]->post_content; ?>
 
     </div>
-
-
 
 <?php else: ?>
 
